a019155489c43c102c1496cc5431750036a7a5de
Ayesha0300 <ayeshabashir030@gmail.com>
2024-05-05 16:45:36 +0500

    first

---
+++ app/Html.tsx
@@ -0,0 +1,49 @@
+"use client"
+export default function Home() {
+    return (
+      <div className="bg-white-400 h-screen">
+        <div className="div">
+          <h1 className=" font-bold">Created By AYESHA BASHIR</h1>
+          <ul>
+  
+            <li className="bg-orange-500">
+              <div className="icon"><i className="fa-brands fa-html5"></i></div>
+              <div className="title">HTML 5</div>
+              <div className="descr">Elements, Div, Flex, Margin, Paddng, Forms
+                +
+                PROJECT</div>
+            </li>
+            <li className="bg-blue-700">
+            
+              <div className="icon"><i className="fa-brands fa-css3"></i></div>
+              <div className="title">CSS 3</div>
+              <div className="descr">Elements, Div, Flex, Margin, Paddng, Forms
+                +
+                PROJECT</div>
+            </li>
+            <li className="bg-green-700">
+              <div className="icon"><i className="fa-brands fa-js"></i></div>
+              <div className="title">Javascript</div>
+              <div className="descr">Functions, Loops, OOPs Concepts + PROJECT</div>
+            </li>
+            <li className="bg-yellow-400">
+              <div className="icon"><i className="fa-brands fa-next.js"></i></div>
+              <div className="title">Next.Js14</div>
+              <div className="descr">Performance,
+                Pre-rendering, Metadata improvements, Server Actions (Stable).
+              </div>
+            </li>
+            <li className="bg-purple-900">
+              <div className="icon"><i className="fa-brands fa-github"></i></div>
+              <div className="title">Github</div>
+              <div className="descr">Store and track software projects,Work together in a centralized Git repository,Track changes to source code,Share code.
+              </div>
+            </li>
+  
+          </ul>
+          <p></p>
+        </div>
+      </div>
+    );
+  };
+  
